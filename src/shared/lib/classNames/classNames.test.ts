5a891c6b6810c6079c486952ecbc5f01b7a74867
Саргсян Майкл Оганесович(4092323) <msargsyan@dev.vtb.ru>
2024-04-04 15:10:50 +0300

    feat(TASK-020): jest config, eslint max length

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,19 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('with only first param', () => {
+        expect(classNames('someClass')).toBe('someClass');
+    });
+
+    test('with additional class', () => {
+        const expected = 'someClass class1 class2';
+
+        expect(classNames('someClass', {}, ['class1', 'class2'])).toBe(expected);
+    });
+
+    test('with additional class and modes', () => {
+        const expected = 'someClass class1 class2 hovered scrollable';
+
+        expect(classNames('someClass', { hovered: true, scrollable: true }, ['class1', 'class2'])).toBe(expected);
+    });
+});
